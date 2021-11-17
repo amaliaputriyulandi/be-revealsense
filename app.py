f28476e1a61d55a8206608e042f5ef0f53c6db8b
Amalia Putri Yulandi <amaliayulandi11@gmail.com>
2021-11-17 19:27:40 +0700

    init project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+#from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+#app.config["SQLALCHEMY_DATABASE_URI"]='postgres'
+
+#db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return "Hello WorlD"
+
+if __name__ == '__main__':
+    app.run(debug=True)
