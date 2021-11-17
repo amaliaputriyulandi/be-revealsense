660517ad06a30bd946c2f29a9da343e859e3ccb8
Amalia Putri Yulandi <amaliayulandi11@gmail.com>
2021-11-17 19:31:17 +0700

    init change

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 #from flask_sqlalchemy import SQLAlchemy
+#melakukan perubahan
 
 app = Flask(__name__)
 CORS(app)
